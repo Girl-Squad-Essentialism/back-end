a6c2421049e0d2d27b9ecc8122c059e7f7988a9b
Barbara Moore <barbaraloismoore@gmail.com>
2021-03-05 11:55:32 -0500

    more file structure work

---
+++ api/server.js
@@ -0,0 +1,22 @@
+const express = require('express');
+const cors = require('cors');
+const helmet = require('helmet');
+
+const authenticate = require('../middleware/user-authentication.js');
+const userRouter = require('../users/users-router.js');
+const projectRouter = require('../projects/projects-router.js');
+
+const server = express();
+
+server.use(helmet());
+server.use(cors());
+server.use(express.json());
+
+server.use('/api/users', userRouter);
+server.use('/api/projects', authenticate, projectRouter);
+
+server.get('/', (req, res) => {
+  res.status(200).json({ api: `it's working, it's working!` });
+});
+
+module.exports = server;
---
+++ users/users-model.test.js
@@ -0,0 +1,54 @@
+const Users = require('./users-model.js');
+const db = require('../data/connection.js');
+
+describe('users model', () => {
+  describe('find method', () => {
+    beforeEach(async () => {
+      await db('users').truncate();
+      await Users.add({ username: 'joe', password: 'garcia' });
+      await Users.add({ username: 'ben', password: 'ransom' });
+    });
+
+    it('should return a list of all users', async () => {
+      const users = await Users.find();
+      expect(users).toHaveLength(2);
+    });
+  });
+
+  describe('findBy method', () => {
+    beforeEach(async () => {
+      await db('users').truncate();
+      await Users.add({ username: 'ben', password: 'ransom' });
+    });
+
+    it('should find the provided user in the database', async () => {
+      const user = await Users.findBy({ username: 'ben' });
+      expect(user).toHaveLength(1);
+    });
+  });
+
+  describe('add method', () => {
+    beforeEach(async () => {
+      await db('users').truncate();
+    });
+
+    it('should insert the provided user into the database', async () => {
+      await Users.add({ username: 'joe', password: 'garcia' });
+      const users = await db('users');
+      expect(users).toHaveLength(1);
+    });
+  });
+
+  describe('findById method', () => {
+    beforeEach(async () => {
+      await db('users').truncate();
+      await Users.add({ username: 'ben', password: 'ransom' });
+      await Users.add({ username: 'tori', password: 'benson' });
+    });
+
+    it('should find the provided user in the database', async () => {
+      const user = await Users.findById(2);
+      expect(user.id).toEqual(2);
+    });
+  });
+});
